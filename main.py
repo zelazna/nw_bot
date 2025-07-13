e55c41d67903f05bbaec8305879f5ab0030b9cff
zelazna <constantin.guidon@gmail.com>
2025-07-13 18:48:42 +0200

    [ALL] spec file and icons

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sys
 
 from PySide6.QtWidgets import QApplication
+from PySide6 import QtGui
 from bot.ui import Widget, MainWindow, style
 
 
 if __name__ == "__main__":
     # Qt Application
     app = QApplication(sys.argv)
+    app.setWindowIcon(QtGui.QIcon("nwbot.ico"))
     # QWidget
     widget = Widget()
     # QMainWindow using QWidget as central widget
---
+++ readme.md
@@ -0,0 +1,17 @@
+## Install
+
+```bash
+pip -r requirements.txt
+```
+
+## Launch It
+
+```bash
+python main.py
+```
+
+## Generate the exec
+
+````bash
+pyinstaller nwbot.spec
+```
