2c9eb312fbdfb490b602fac5023f053cb77ec100
zelazna <constantin.guidon@gmail.com>
2025-07-14 14:31:22 +0200

    [UI] Fix missing icon

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import sys
+import os
 
 from PySide6.QtWidgets import QApplication
 from PySide6 import QtGui
 from bot.ui import Widget, MainWindow, style
+from ctypes import windll
 
+basedir = os.path.dirname(__file__)
+
+windll.shell32.SetCurrentProcessExplicitAppUserModelID("nwbot")
 
 if __name__ == "__main__":
     # Qt Application
     app = QApplication(sys.argv)
-    app.setWindowIcon(QtGui.QIcon("nwbot.ico"))
+    app.setWindowIcon(QtGui.QIcon(os.path.join(basedir,"nwbot.ico")))
     # QWidget
     widget = Widget()
     # QMainWindow using QWidget as central widget
--- nwbot.spec
+++ nwbot.spec
@@ -35,5 +35,5 @@ exe = EXE(
     target_arch=None,
     codesign_identity=None,
     entitlements_file=None,
-    icon=['nwbot.ico'],
+    icon='nwbot.ico',
 )
